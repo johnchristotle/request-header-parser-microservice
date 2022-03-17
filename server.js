91652aefeb3f4754965634fa7cceda5354001b6f
agholor-christotle <agholorchristotle@gmail.com>
2022-03-17 12:36:40 -0700

    project main page completed and tested

---
+++ server.js
@@ -0,0 +1,43 @@
+// server.js
+// This is where your node app starts from
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+
+let responseObject = {}
+app.enable('trust proxy')
+app.get('/api/whoami', (request, response) => {
+  responseObject['ipaddress'] = request.ip
+  responseObject['language'] = request.get('Accept-Language')
+  responseObject['software'] = request.get('User-Agent')
+  
+  response.json(responseObject)
+})
